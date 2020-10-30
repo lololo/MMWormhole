b5c7c0043a38cea7e3e127169a32836bf126472d
lei <guolei@me.com>
2020-10-30 16:11:20 +0800

    support swift package manager

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "MMWormhole",
+    platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)],
+    products:[
+        .library(name:"MMWormhole", targets:["MMWormhole"])
+    ],
+    targets:[
+        .target(name:"MMWormhole", path:"Source")
+    ],
+    swiftLanguageVersions: [.v5]
+)
